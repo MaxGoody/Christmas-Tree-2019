63d86ae209ee254b9e796f48848dd648c00f1109
Maxim Alexeev <maksimgoody@gmail.com>
2018-12-25 20:24:47 +0300

    Sender

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,133 @@
 <?php
 require __DIR__.'/src/exceptions.php';
 require __DIR__.'/src/autoload.php';
+
+use Application\Requests\Sender;
+
+$sender = new Sender();
+
+# Авторизация
+$response = $sender->sendUnsigned('canvas', 'show', [
+    'api_id' => 6743634,
+    'viewer_id' => 0,
+    'auth_key' => ''
+]);
+
+$parameters = [
+    'aid' => $response['appId'],
+    'uid' => $response['uid'],
+    'suid' => $response['suid'],
+    'authKey' => $response['authKey'],
+    'sessionKey' => $response['sessionKey'],
+    'version' => $response['version'],
+    'clientPlatform' => 'js'
+];
+
+# Инициализация
+$playerInfo = $sender->sendSigned('game', 'init', $parameters, [
+    'friendIds' => [],
+    'sessionData' => [
+        'sessionId' => $response['fluentSession']
+    ]
+]);
+
+$parameters['sessionKey'] = $playerInfo['user']['session'];
+
+# Принимаем почту.
+$count = count($playerInfo['mail']['inbox']);
+if ($count !== 0) {
+    $sender->sendSigned('mail', 'flushInbox', $parameters);
+}
+
+# Отправляем почту.
+$count = count($playerInfo['mail']['requests']);
+if ($count !== 0) {
+    $sender->sendSigned('mail', 'flushRequests', $parameters);
+}
+
+# Собираем генераторы.
+foreach ($playerInfo['user']['generators'] as $name => $data) {
+    $sender->sendSigned('generator', 'exchange', $parameters, [
+        'name' => $name
+    ]);
+}
+
+# Собираем прибыль с эльфов.
+if (count($playerInfo['user']['elf']['collect']) !== 0) {
+    $sender->sendSigned('Elf', 'UnloadFromCollect', $parameters);
+}
+
+# Запускаем эльфов.
+foreach ($playerInfo['user']['elf']['data'] as $id => $data) {
+    if ($data['type'] !== 2 && $data['time'] === 0) {
+        $sender->sendSigned('Elf', 'Start', [
+            'elfId' => $id
+        ]);
+    }
+}
+
+# Собираем квесты.
+foreach ($playerInfo['user']['quest'] as $id => $data) {
+    if (isset($data['tasks']) === false) {
+        continue;
+    }
+
+    foreach ($data['tasks'] as $taskId => $value) {
+        if (isset($data['progress'][$taskId]) === false) {
+            continue;
+        }
+
+        if ($data['progress'][$taskId] === $value) {
+            $sender->sendSigned('Quest', 'Finish', $parameters, [
+                'questLineId' => $id
+            ]);
+        }
+    }
+}
+
+# Собираем сундучки.
+foreach ($playerInfo['friendsList'] as $id => $data) {
+    if (isset($data['hash']) === false) {
+        continue;
+    }
+
+    $response = $sender->sendSigned('friend', 'get', $parameters, [
+        'screenId' => $playerInfo['user']['screen'],
+        'sFriendId' => $id,
+        'chestId' => 1
+    ]);
+
+    if (time() - $response['user']['loginTime'] >= 2592000 ||
+        isset($response['chest'][1]) === false ||
+        $response['chest'][1]['time'] !== 0) {
+        continue;
+    }
+
+    $sender->sendSigned('chest', 'open', $parameters, [
+        'chestId' => 1,
+        'screenId' => $playerInfo['user']['screen'],
+        'sFriendId' => $id,
+        'hash' => $data['hash']
+    ]);
+
+}
+
+# Собираем палочки.
+# TODO: Объеденить с верхним циклом.
+$count = count($playerInfo['friendsData']['energyGather']);
+$friends = [];
+
+foreach ($playerInfo['friendsList'] as $id => $data) {
+    if (count($friends) + $count === 50 || isset($data['hash']) === false || $data['allowEnergyGather'] !== 1) {
+        continue;
+    }
+    
+    $friends[$id] = $data['hash'];
+}
+
+foreach (array_chunk($friends, 10, true) as $chunk) {
+    $sender->sendSigned('friend', 'getEnergy', $parameters, [
+        'screenId' => $playerInfo['user']['screen'],
+        'friendsData'=> $chunk
+    ]);
+}
---
+++ src/Application/Requests/Sender.php
@@ -0,0 +1,135 @@
+<?php
+namespace Application\Requests;
+
+use RuntimeException;
+use InvalidArgumentException;
+
+/**
+ * Sender
+ * @package Application\Requests
+ */
+final class Sender
+{
+    /**
+     * URL Сервера.
+     */
+    public const SERVER_URL = 'https://elka2019-server-vk.ereality.org';
+
+    /**
+     * Стандартные заголовки.
+     */
+    public const DEFAULT_HEADERS = [
+        'Accept: */*',
+        'Accept-Language: ru,en;q=0.9',
+        'Connection: keep-alive',
+        'Origin: https://elka2019-client-vk.ereality.org',
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    ];
+
+    /**
+     * Отправить подписанный запрос.
+     * @param string $controller
+     * @param string $action
+     * @param array $parameters
+     * @param array|null $requestParameters
+     * @return mixed
+     */
+    public function sendSigned(
+        string $controller,
+        string $action,
+        array $parameters,
+        ?array $requestParameters = null
+    ) {
+        $body = $parameters;
+        $body['params'] = $requestParameters ?? [];
+        $body['sign'] = Signature::generate($controller, $action, $parameters, $requestParameters);
+        $body = json_encode($body);
+
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => self::SERVER_URL."/{$controller}/{$action}",
+            CURLOPT_POST => true,
+            CURLOPT_HTTPHEADER => array_merge(self::DEFAULT_HEADERS, [
+                'Content-Length: '.mb_strlen($body),
+                'Content-Type: application/json'
+            ]),
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => $body
+        ]);
+
+        $response = curl_exec($curl);
+        if ($response === false) {
+            throw new RuntimeException(curl_error($curl), curl_errno($curl));
+        }
+
+        curl_close($curl);
+
+        $response = json_decode($response, true, JSON_THROW_ON_ERROR);
+        if (isset($response['error']) === true) {
+            throw new RuntimeException($response['error']['text'], $response['error']['code']);
+        }
+
+        return $response['data'] ?? $response;
+    }
+
+    /**
+     * Отправить запрос без подписи.
+     * @param string $controller
+     * @param string $action
+     * @param array|null $query
+     * @return mixed
+     */
+    public static function sendUnsigned(string $controller, string $action, ?array $query = null)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => self::SERVER_URL."/{$controller}/{$action}"
+                .($query === null ? '' : '?'.http_build_query($query)),
+            CURLOPT_HTTPHEADER => self::DEFAULT_HEADERS,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_RETURNTRANSFER => true
+        ]);
+
+        $response = curl_exec($curl);
+        if ($response === false) {
+            throw new RuntimeException(curl_error($curl), curl_errno($curl));
+        }
+
+        curl_close($curl);
+
+        $response = json_decode($response, true, JSON_THROW_ON_ERROR);
+        if (isset($response['error']) === true) {
+            throw new RuntimeException($response['error']['text'], $response['error']['code']);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Разобрать настйроки.
+     * @param array $settings
+     * @return array
+     */
+    private function parseSettings(array $settings): array
+    {
+        $newSettings = [];
+        $newSettings['method'] = $settings['method'] ?? 'POST';
+        $newSettings['secure'] = $settings['secure'] ?? true;
+        $newSettings['parameters'] = $settings['parameters'] ?? [];
+
+        if (isset($settings['controller']) === false) {
+            throw new InvalidArgumentException('Не указан контроллер (controller)!');
+        }
+        $newSettings['controller'] = trim($settings['controller'], '/');
+
+        if (isset($settings['action']) === false) {
+            throw new InvalidArgumentException('Не указано действие (action)!');
+        }
+        $newSettings['action'] = trim($settings['action'], '/');
+
+        return $newSettings;
+    }
+}
